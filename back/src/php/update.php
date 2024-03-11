80437d792057df44c596d6d87960468528205c2c
Eduarda S. Rech <dudasrech@icloud.com>
2024-03-11 14:07:40 -0300

    Update update.php

--- back/src/php/update.php
+++ back/src/php/update.php
@@ -2,16 +2,21 @@
 header(header:'Access-Control-Allow-Origin: *');
 header(header:'Access-Control-Allow-Headers: content-type, *');
 header(header:'Access-Control-Allow-Methods: *');
-include_once "connection.php";
 
 /* Update */
-    $page=$_GET["page"];
+function updateValues() {
+    $host = "pgsql_desafio";
+    $db = "applicationphp";
+    $user = "root";
+    $pw = "root";
+    $connection = new PDO("pgsql:host=$host;dbname=$db", $user, $pw);
+    
     $action=$_GET["action"];
     $newvalue=$_GET["newvalue"];
     $column=$_GET["column"];
     $value=$_GET["value"];
 
-    $sql = "UPDATE ${page} SET {$action}={$newvalue} WHERE {$column}='{$value}'";
+    $sql = "UPDATE products SET {$action}={$newvalue} WHERE {$column}='{$value}'";
     $result = $connection->query($sql);
 
     $data = [];
@@ -20,5 +25,7 @@ include_once "connection.php";
     }
 
     echo json_encode($data);
+};
+updateValues();
 
 ?>
